c8212c27cb5efca5768ea051b5c9175a83e10b85
Peter Seibel <peter@gigamonkeys.com>
2024-12-01 11:00:07 -0800

    Usage message if not enough command line args provided.

--- projects/advent-of-code/src/net/berkeley/students/peterseibel/AdventOfCode.java
+++ projects/advent-of-code/src/net/berkeley/students/peterseibel/AdventOfCode.java
@@ -11,12 +11,15 @@ public class AdventOfCode {
   public static void main(String[] args) throws IOException {
     System.out.println("Welcome to Advent of Code!");
 
-    var day = Integer.parseInt(args[0]);
-    var part = Integer.parseInt(args[1]);
+    if (args.length < 2) {
+      System.out.println("Provide day and part to run code.");
+    } else {
+      var day = Integer.parseInt(args[0]);
+      var part = Integer.parseInt(args[1]);
 
-    var d = Day.number(day);
-
-    d.part(part, true);
-    d.part(part, false);
+      var d = Day.number(day);
+      d.part(part, true);
+      d.part(part, false);
+    }
   }
 }
