34135f8060276e796b4fa16697fba06339d999e6
Peter Seibel <peter@gigamonkeys.com>
2024-12-01 07:00:55 -0800

    Get I/O going.

--- projects/advent-of-code/src/net/berkeley/students/peterseibel/AdventOfCode.java
+++ projects/advent-of-code/src/net/berkeley/students/peterseibel/AdventOfCode.java
@@ -1,7 +1,12 @@
 package net.berkeley.students.peterseibel;
 
+import java.io.*;
+import java.nio.file.*;
+import static java.nio.file.Files.lines;
+
 public class AdventOfCode {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     System.out.println("Welcome to Advent of Code!");
+    lines(Path.of("inputs/day-01-test.txt")).forEach(System.out::println);
   }
 }
