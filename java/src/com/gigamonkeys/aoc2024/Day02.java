ff39c7dce450cd5078b35650f2ea7b486c2e1e2d
Peter Seibel <peter@gigamonkeys.com>
2024-12-02 07:17:21 -0800

    Day 2, part 2.

--- java/src/com/gigamonkeys/aoc2024/Day02.java
+++ java/src/com/gigamonkeys/aoc2024/Day02.java
@@ -5,12 +5,15 @@ import static java.nio.file.Files.lines;
 import static java.util.stream.Collectors.*;
 import static java.lang.Math.*;
 
+import java.util.function.*;
 import java.io.*;
 import java.nio.file.*;
 import java.util.*;
 import java.util.stream.*;
 
 public class Day02 extends Day {
+
+  private static final IntPredicate GAP_OKAY = n -> 1 <= abs(n) && abs(n) <= 3;
 
   public Day02() {
     super(2);
@@ -21,19 +24,30 @@ public class Day02 extends Day {
   }
 
   public String part1(boolean test) throws IOException {
+    return String.valueOf(data(test).stream().filter(this::isStrictlySafe).count());
+  }
+
+  public String part2(boolean test) throws IOException {
     return String.valueOf(data(test).stream().filter(this::isSafe).count());
   }
 
-
-  public String part2(boolean test) throws IOException {
-    return "";
+  private boolean isSafe(int[] levels) {
+    if (isStrictlySafe(levels)) return true;
+    return IntStream.range(0, levels.length)
+      .mapToObj(i -> without(levels, i))
+      .anyMatch(this::isStrictlySafe);
   }
 
-  private boolean isSafe(int[] levels) {
-    var deltas = IntStream.range(0, levels.length - 1).map(i -> levels[i] - levels[i + 1]).toArray();
-    return (
-      Arrays.stream(deltas).allMatch(d -> Math.signum(d) == Math.signum(deltas[0])) &&
-      Arrays.stream(deltas).map(Math::abs).allMatch(d -> 1 <= d && d <= 3)
-    );
+  private int[] without(int[] levels, int idx) {
+    return IntStream.range(0, levels.length).filter(i -> i != idx).map(i -> levels[i]).toArray();
+  }
+
+  private boolean isStrictlySafe(int[] levels) {
+    var deltas = deltas(levels);
+    return Arrays.stream(deltas).allMatch(GAP_OKAY.and(d -> signum(d) == signum(deltas[0])));
+  }
+
+  private int[] deltas(int[] levels) {
+    return IntStream.range(0, levels.length - 1).map(i -> levels[i] - levels[i + 1]).toArray();
   }
 }
