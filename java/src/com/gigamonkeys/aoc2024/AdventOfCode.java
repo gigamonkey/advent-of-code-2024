587e627acbede39d02077abbffc09831ae3fb498
Peter Seibel <peter@gigamonkeys.com>
2024-12-05 06:41:40 -0800

    Hollowing out Day class.

--- java/src/com/gigamonkeys/aoc2024/AdventOfCode.java
+++ java/src/com/gigamonkeys/aoc2024/AdventOfCode.java
@@ -1,7 +1,12 @@
 package com.gigamonkeys.aoc2024;
 
+import static java.nio.file.Files.exists;
+import static java.nio.file.Files.lines;
+import static java.nio.file.Files.readString;
 import static java.time.temporal.ChronoUnit.DAYS;
 
+import java.nio.file.*;
+import java.util.*;
 import java.io.IOException;
 import java.time.*;
 
@@ -13,20 +18,90 @@ public class AdventOfCode {
 
   private static final int MAX_DAY = (int) DAYS.between(START, NOW) + 1;
 
-  public static void main(String[] args) throws IOException {
-    System.out.println("Welcome to Advent of Code!");
+  public static final List<Day> days = new ArrayList<>();
 
-    int start = args.length > 0 && args[0].equals("--all") ? 1 : MAX_DAY;
+  static {
+    days.add(new Day01());
+    days.add(new Day02());
+    days.add(new Day03());
+    days.add(new Day04());
+    days.add(new Day05());
+  }
+
+  private Day number(int day) {
+    if ((day - 1) < days.size()) {
+      return days.get(day - 1);
+    } else {
+      throw new RuntimeException("Day %d not implemented yet!".formatted(day));
+    }
+  }
+
+  public boolean runPart(Day day, int part, boolean test) throws IOException {
+    String result =
+      (switch (part) {
+        case 1 -> day.part1(input(day.day(), part, test));
+        case 2 -> day.part2(input(day.day(), part, test));
+        default -> "No part " + part;
+      }).trim();
+
+    Optional<String> expected = expected(day.day(), part, test);
+
+    if (expected.isPresent()) {
+      var e = expected.get();
+      if (e.equals(result)) {
+        System.out.printf("Day %d, part %d: %s - ok!%n", day.day(), part, result);
+        return true;
+      } else {
+        System.out.printf("Day %d, part %d: %s - Ooops. Expected: %s%n", day.day(), part, result, e);
+      }
+    } else {
+      System.out.printf("Day %d, part %d: %s - no expected value yet.%n", day.day(), part, result);
+    }
+    return false;
+  }
+
+  /**
+   * Get the Path of the input file for either the test or real input.
+   */
+  private Path input(int day, int part, boolean test) {
+    // Special case for some days that have different test input for part 2
+    if (test && part == 2) {
+      var p2 = Path.of("inputs/day-%02d/test2.txt".formatted(day));
+      if (exists(p2)) return p2;
+    }
+
+    // The normal case.
+    return Path.of("inputs/day-%02d/%s.txt".formatted(day, test ? "test" : "real"));
+  }
+
+  private Optional<String> expected(int day, int part, boolean test) throws IOException {
+    var p = Path.of("inputs/day-%02d/part-%d%s.expected".formatted(day, part, test ? "-test" : ""));
+    return exists(p) ? Optional.of(readString(p).trim()) : Optional.empty();
+  }
+
+  public boolean run(int start) throws IOException {
 
     boolean okay = true;
 
     for (var day = start; day <= MAX_DAY; day++) {
       for (var part = 1; part <= 2; part++) {
-        var d = Day.number(day);
-        okay &= d.part(part, true);
-        okay &= d.part(part, false);
+        var d = number(day);
+        okay &= runPart(d, part, true);
+        okay &= runPart(d, part, false);
       }
     }
+
+    return okay;
+  }
+
+
+  public static void main(String[] args) throws IOException {
+    System.out.println("Welcome to Advent of Code!");
+
+    int start = args.length > 0 && args[0].equals("--all") ? 1 : MAX_DAY;
+    var aoc = new AdventOfCode();
+    boolean okay = aoc.run(start);
+
     if (okay) {
       System.out.println("\nAll okay!");
     } else {
--- java/src/com/gigamonkeys/aoc2024/Day.java
+++ java/src/com/gigamonkeys/aoc2024/Day.java
@@ -10,29 +10,13 @@ import java.util.*;
 
 public abstract class Day {
 
-  private static final List<Day> days = new ArrayList<>();
-
-  static {
-    days.add(new Day01());
-    days.add(new Day02());
-    days.add(new Day03());
-    days.add(new Day04());
-    days.add(new Day05());
-  }
-
   private final int day;
 
   public Day() {
-    this.day = days.size() + 1;
+    this.day = AdventOfCode.days.size() + 1;
   }
 
-  public static Day number(int day) {
-    if ((day - 1) < days.size()) {
-      return days.get(day - 1);
-    } else {
-      throw new RuntimeException("Day %d not implemented yet!".formatted(day));
-    }
-  }
+  public int day() { return day; }
 
   /**
    * Solve part1 for the given input.
@@ -44,46 +28,4 @@ public abstract class Day {
    */
   public abstract String part2(Path input) throws IOException;
 
-  public boolean part(int part, boolean test) throws IOException {
-    String result =
-      (switch (part) {
-          case 1 -> part1(input(part, test));
-          case 2 -> part2(input(part, test));
-          default -> "No part " + part;
-        }).trim();
-
-    Optional<String> expected = expected(part, test);
-
-    if (expected.isPresent()) {
-      var e = expected.get();
-      if (e.equals(result)) {
-        System.out.printf("Day %d, part %d: %s - ok!%n", day, part, result);
-        return true;
-      } else {
-        System.out.printf("Day %d, part %d: %s - Ooops. Expected: %s%n", day, part, result, e);
-      }
-    } else {
-      System.out.printf("Day %d, part %d: %s - no expected value yet.%n", day, part, result);
-    }
-    return false;
-  }
-
-  /**
-   * Get the Path of the input file for either the test or real input.
-   */
-  private Path input(int part, boolean test) {
-    // Special case for some days that have different test input for part 2
-    if (test && part == 2) {
-      var p2 = Path.of("inputs/day-%02d/test2.txt".formatted(day));
-      if (exists(p2)) return p2;
-    }
-
-    // The normal case.
-    return Path.of("inputs/day-%02d/%s.txt".formatted(day, test ? "test" : "real"));
-  }
-
-  private Optional<String> expected(int part, boolean test) throws IOException {
-    var p = Path.of("inputs/day-%02d/part-%d%s.expected".formatted(day, part, test ? "-test" : ""));
-    return exists(p) ? Optional.of(readString(p).trim()) : Optional.empty();
-  }
 }
