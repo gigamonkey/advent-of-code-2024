65050fb6f2795f451ac63c889c880db3fb93fdf2
Peter Seibel <peter@gigamonkeys.com>
2024-12-04 22:14:45 -0800

    Some code golf.

--- java/src/com/gigamonkeys/aoc2024/Day05.java
+++ java/src/com/gigamonkeys/aoc2024/Day05.java
@@ -6,6 +6,7 @@ import static java.nio.file.Files.lines;
 import static java.util.Collections.emptySet;
 import static java.util.regex.Pattern.compile;
 import static java.util.stream.Collectors.*;
+import static java.util.stream.IntStream.range;
 
 import java.io.*;
 import java.nio.file.*;
@@ -16,18 +17,18 @@ import java.util.stream.*;
 public class Day05 extends Day {
 
   public String part1(Path input) throws IOException {
-    var preceeding = preceeding(input);
-    return String.valueOf(updates(input).stream().filter(u -> ordered(u, preceeding)).mapToInt(this::middle).sum());
+    var mustPreceed = mustPreceed(input);
+    var cmp = byOrderingRules(mustPreceed);
+    return String.valueOf(updates(input).stream().filter(u -> inOrder(u, cmp)).mapToInt(this::middle).sum());
   }
 
   public String part2(Path input) throws IOException {
-    var preceeding = preceeding(input);
-    var cmp = byOrderingRules(preceeding);
-
+    var mustPreceed = mustPreceed(input);
+    var cmp = byOrderingRules(mustPreceed);
     return String.valueOf(
       updates(input)
         .stream()
-        .filter(u -> !ordered(u, preceeding))
+        .filter(u -> !inOrder(u, cmp))
         .map(lst -> lst.stream().sorted(cmp).toList())
         .mapToInt(this::middle)
         .sum()
@@ -35,35 +36,23 @@ public class Day05 extends Day {
   }
 
   private Comparator<Integer> byOrderingRules(Map<Integer, Set<Integer>> mustPreceed) {
-    return (a, b) -> {
-      if (mustPreceed.getOrDefault(a, emptySet()).contains(b)) {
-        return -1;
-      } else if (mustPreceed.getOrDefault(b, emptySet()).contains(a)) {
-        return 1;
-      } else {
-        return 0;
-      }
-    };
+    return (a, b) ->
+      mustPreceed.getOrDefault(a, emptySet()).contains(b)
+        ? -1
+        : mustPreceed.getOrDefault(b, emptySet()).contains(a)
+        ? 1
+        : 0;
   }
 
-  private boolean ordered(List<Integer> u, Map<Integer, Set<Integer>> mustPreceed) {
-    var seen = new HashSet<Integer>();
-    for (int p : u) {
-      var mp = mustPreceed.getOrDefault(p, emptySet()); // pages that we must come before
-      if (seen.stream().anyMatch(p2 -> mp.contains(p2))) {
-        // Pages that have already been printed include pages we must come before.
-        return false;
-      }
-      seen.add(p);
-    }
-    return true;
+  private <T> boolean inOrder(List<T> list, Comparator<T> cmp) {
+    return range(0, list.size() - 1).allMatch(i -> cmp.compare(list.get(i), list.get(i + 1)) < 0);
   }
 
   private <T> T middle(List<T> list) {
     return list.get(list.size() / 2);
   }
 
-  private Map<Integer, Set<Integer>> preceeding(Path input) throws IOException {
+  private Map<Integer, Set<Integer>> mustPreceed(Path input) throws IOException {
     return lines(input)
       .filter(line -> line.matches("\\d+\\|\\d+"))
       .map(line -> Arrays.stream(line.split("\\|")).map(Integer::valueOf).toList())
