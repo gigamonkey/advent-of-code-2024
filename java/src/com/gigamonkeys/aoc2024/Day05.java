bbf961870955d2e17f9b4784e5df502d1d03ebc3
Peter Seibel <peter@gigamonkeys.com>
2024-12-04 16:46:15 -0800

    Day 5 starter code.

---
+++ java/src/com/gigamonkeys/aoc2024/Day05.java
@@ -0,0 +1,24 @@
+package com.gigamonkeys.aoc2024;
+
+import static com.gigamonkeys.aoc2024.Util.text;
+import static java.lang.Integer.*;
+import static java.nio.file.Files.lines;
+import static java.util.regex.Pattern.compile;
+
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
+import java.util.regex.*;
+import java.util.stream.*;
+
+public class Day05 extends Day {
+
+  public String part1(Path input) throws IOException {
+    return "NYI";
+  }
+
+  public String part2(Path input) throws IOException {
+    return "NYI";
+  }
+
+}
