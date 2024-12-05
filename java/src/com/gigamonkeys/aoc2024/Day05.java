40023574218eff210d8b66cab4ea76d85debcf8c
Peter Seibel <peter@gigamonkeys.com>
2024-12-04 21:40:15 -0800

    Day 5, part 2. Working but gross.

--- java/src/com/gigamonkeys/aoc2024/Day05.java
+++ java/src/com/gigamonkeys/aoc2024/Day05.java
@@ -1,5 +1,6 @@
 package com.gigamonkeys.aoc2024;
 
+import static java.util.Collections.EMPTY_SET;
 import static com.gigamonkeys.aoc2024.Util.*;
 import static java.lang.Integer.*;
 import static java.nio.file.Files.lines;
@@ -23,6 +24,36 @@ public class Day05 extends Day {
     );
   }
 
+  public String part2(Path input) throws IOException {
+    var preceeding = preceeding(input);
+    var updates = updates(input);
+
+    var unordered = updates.stream().filter(u -> !ordered(u, preceeding)).toList();
+    //System.out.println(unordered);
+
+    var cmp = byOrder(preceeding);
+    return String.valueOf(
+      unordered.stream().map(lst -> {
+          var srt = new ArrayList<Integer>(lst);
+          Collections.sort(srt, cmp);
+          return srt;
+        }).mapToInt(this::middle).sum()
+    );
+
+  }
+
+  private Comparator<Integer> byOrder(Map<Integer, Set<Integer>> mustPreceed) {
+    return (a, b) -> {
+      if (mustPreceed.getOrDefault(a, EMPTY_SET).contains(b)) {
+        return -1;
+      } else if (mustPreceed.getOrDefault(b, EMPTY_SET).contains(a)) {
+        return 1;
+      } else {
+        return 0;
+      }
+    };
+  }
+
   private boolean ordered(List<Integer> u, Map<Integer, Set<Integer>> mustPreceed) {
     var seen = new HashSet<Integer>();
     for (int p: u) {
@@ -38,10 +69,6 @@ public class Day05 extends Day {
 
   private <T> T middle(List<T> list) {
     return list.get(list.size() / 2);
-  }
-
-  public String part2(Path input) throws IOException {
-    return "NYI";
   }
 
   private Map<Integer, Set<Integer>> preceeding(Path input) throws IOException {
