e6321290c5c461b6228d93429134017461f62ff1
Peter Seibel <peter@gigamonkeys.com>
2024-12-06 20:29:02 -0800

    Getting set up for day 7.

---
+++ java/src/com/gigamonkeys/aoc2024/Day07.java
@@ -0,0 +1,19 @@
+package com.gigamonkeys.aoc2024;
+
+import static com.gigamonkeys.aoc2024.Util.*;
+
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
+import java.util.stream.*;
+
+public class Day07 implements Solution {
+
+  public String part1(Path input) throws IOException {
+    return String.valueOf("nyi");
+  }
+
+  public String part2(Path input) throws IOException {
+    return String.valueOf("nyi");
+  }
+}
