cb57f8bec73394e9cf4d0d914362f202e6b23729
Peter Seibel <peter@gigamonkeys.com>
2024-12-07 11:59:02 -0800

    Use long instead of BigInteger

--- java/src/com/gigamonkeys/aoc2024/BridgeRepair.java
+++ java/src/com/gigamonkeys/aoc2024/BridgeRepair.java
@@ -3,7 +3,6 @@ package com.gigamonkeys.aoc2024;
 import static java.nio.file.Files.*;
 
 import java.io.IOException;
-import java.math.BigInteger;
 import java.nio.file.Path;
 import java.util.*;
 import java.util.function.BinaryOperator;
@@ -12,18 +11,17 @@ import java.util.stream.Stream;
 
 public class BridgeRepair implements Solution {
 
-  private static final List<Op> ALL_OPS = List.of(
-        new Op(BigInteger.ZERO, BigInteger::add),
-        new Op(BigInteger.ONE, BigInteger::multiply),
-        new Op(BigInteger.ZERO, BridgeRepair::concat)
-    );
+  record Equation(long value, List<Long> numbers) {}
 
+  record Op(long identity, BinaryOperator<Long> op) {}
+
+  private static final List<Op> ALL_OPS = List.of(
+    new Op(0, (a, b) -> a + b),
+    new Op(1, (a, b) -> a * b),
+    new Op(0, BridgeRepair::concat)
+  );
 
   Pattern p = Pattern.compile("^(\\d+):\\s+(\\d+(\\s+\\d+)*)$");
-
-  record Equation(BigInteger value, List<BigInteger> numbers) {}
-
-  record Op(BigInteger identity, BinaryOperator<BigInteger> op) {}
 
   public String part1(Path input) throws IOException {
     return solve(input, ALL_OPS.subList(0, 2));
@@ -34,40 +32,43 @@ public class BridgeRepair implements Solution {
   }
 
   private String solve(Path input, List<Op> ops) throws IOException {
-    return lines(input)
-      .map(this::parseLine)
-      .filter(eq -> check(eq.value(), eq.numbers(), Optional.empty(), ops))
-      .map(Equation::value)
-      .reduce(BigInteger.ZERO, BigInteger::add)
-      .toString();
+    return String.valueOf(
+      lines(input)
+        .map(this::parseLine)
+        .filter(eq -> check(eq.value(), eq.numbers(), Optional.empty(), ops))
+        .mapToLong(Equation::value)
+        .sum()
+    );
   }
 
-  private boolean check(BigInteger value, List<BigInteger> nums, Optional<BigInteger> soFar, List<Op> ops) {
+  private boolean check(long value, List<Long> nums, Optional<Long> soFar, List<Op> ops) {
     if (nums.size() == 0) {
       return soFar.map(n -> n.equals(value)).orElse(false);
     } else {
       var rest = nums.subList(1, nums.size());
-      return ops.stream().anyMatch(op -> {
+      return ops
+        .stream()
+        .anyMatch(op -> {
           return check(
             value,
             rest,
             soFar.or(() -> Optional.of(op.identity())).map(n -> op.op().apply(n, nums.get(0))),
-            ops);
+            ops
+          );
         });
     }
   }
 
-
-  private static BigInteger concat(BigInteger a, BigInteger b) {
-    return new BigInteger(a.toString() + b.toString());
+  private static long concat(long a, long b) {
+    return Long.valueOf("" + a + b);
   }
 
   private Equation parseLine(String line) {
     Matcher m = p.matcher(line);
     if (m.matches()) {
       return new Equation(
-        new BigInteger(m.group(1)),
-        Arrays.stream(m.group(2).split("\\s+")).map(BigInteger::new).toList()
+        Long.valueOf(m.group(1)),
+        Arrays.stream(m.group(2).split("\\s+")).map(Long::valueOf).toList()
       );
     } else {
       throw new RuntimeException("Bad line: '%s'".formatted(line));
