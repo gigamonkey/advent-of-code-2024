bc4c21240605bf40ff18f92afbef400aed9377a0
Peter Seibel <peter@gigamonkeys.com>
2024-12-15 12:50:35 -0800

    More flailing.

--- java/src/com/gigamonkeys/aoc2024/Day13_ClawContraption.java
+++ java/src/com/gigamonkeys/aoc2024/Day13_ClawContraption.java
@@ -22,8 +22,6 @@ import static java.util.Map.entry;
 import static java.util.function.Function.*;
 import static java.util.regex.Pattern.*;
 import static java.util.stream.Collectors.*;
-//import static java.util.stream.IntStream.*;
-import java.util.stream.LongStream;
 import static java.util.stream.LongStream.range;
 import static java.util.stream.LongStream.rangeClosed;
 import static java.util.stream.Stream.*;
@@ -56,6 +54,8 @@ import java.util.regex.*;
 import java.util.regex.MatchResult;
 import java.util.stream.*;
 import java.util.stream.IntStream;
+//import static java.util.stream.IntStream.*;
+import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
 public class Day13_ClawContraption implements Solution {
@@ -86,40 +86,91 @@ public class Day13_ClawContraption implements Solution {
 
     //System.out.println(winner3(machines.get(2)));
 
-    var machine = new Machine(24, 97, 48, 13, 6624, 10120);
+    // var machine = new Machine(12, 54, 36, 27, 2052, 3024);
 
-    System.out.println(winner4(machine));
+    var machine = new Machine(90, 51, 30, 59, 8370, 5625);
+    System.out.println(machine + ": " + winner2(machine));
+    System.out.println(machine + ": " + winner5(machine));
+    System.exit(1);
+
+    //var machine = new Machine(24, 97, 48, 13, 6624, 10120);
+    //System.out.println(winner4(new Machine(24, 97, 48, 13, 6624, 10120)));
+    //System.out.println(winner4(new Machine(94, 34, 22, 67, 8400, 5400)));
 
     //return String.valueOf(machines.stream().map(m -> winner2(m)).mapToLong(op -> op.map(Presses::cost).orElse(0L)).sum());
 
-    //System.out.println("working");
-    //machines.stream().map(m -> winner2(m)).forEach(System.out::println);
-    //System.out.println("not working");
-    //machines.stream().map(m -> winner3(m)).forEach(System.out::println);
+    // System.out.println("working");
+    // machines.stream().forEach(m -> { System.out.println("%s %s".formatted(m, winner2(m))); });
+    // System.out.println("not working");
+    // machines.stream().forEach(m -> { System.out.println("%s %s".formatted(m, winner4(m))); });
+
+    machines
+      .stream()
+      .forEach(m -> {
+        var w2 = winner2(m);
+        var w4 = winner5(m);
+        if (w2.equals(w4)) {} else {
+          System.out.println("FAIL %s got %s; expected: %s".formatted(m, w4, w2));
+        }
+      });
 
     //return String.valueOf(machines.stream().flatMap(m -> winner3(m).stream()).mapToLong(Presses::cost).sum());
 
     //return String.valueOf(machines.stream().map(m -> winner3(m)).mapToLong(op -> op.map(Presses::cost).orElse(0L)).sum());
     return String.valueOf(
-      machines.stream().map(m ->  {
-          return winner3(m);
+      machines
+        .stream()
+        .map(m -> {
+          return winner4(m);
         })
-      .mapToLong(op -> op.map(Presses::cost).orElse(0L))
-      .sum());
-
-
+        .mapToLong(op -> op.map(Presses::cost).orElse(0L))
+        .sum()
+    );
   }
 
   public String part2(Path input) throws IOException {
     var machines = embiggen(machines(input));
     //return String.valueOf(machines.stream().map(m -> winner2(m)).mapToLong(op -> op.map(Presses::cost).orElse(0L)).sum());
     return String.valueOf(
-      machines.stream().map(m ->  {
+      machines
+        .stream()
+        .map(m -> {
           System.out.println(m);
           return winner3(m);
         })
-      .mapToLong(op -> op.map(Presses::cost).orElse(0L))
-      .sum());
+        .mapToLong(op -> op.map(Presses::cost).orElse(0L))
+        .sum()
+    );
+  }
+
+  private Optional<Presses> winner5(Machine m) {
+    Iterator<Presses> xs = orderedPresses(m.ax, m.bx, m.x).iterator();
+    Iterator<Presses> ys = orderedPresses(m.ay, m.by, m.y).iterator();
+
+    if (!xs.hasNext() || !ys.hasNext()) {
+      return Optional.empty();
+    }
+
+    Presses x = xs.next();
+    Presses y = ys.next();
+    while (true) {
+      System.out.println("Comparing " + x + " (cost: " + x.cost() + ") and " + y + " (cost: " + y.cost() + ")");
+      if (x.cost() < y.cost()) {
+        if (!xs.hasNext()) return Optional.empty();
+        x = xs.next();
+      } else if (y.cost() < x.cost()) {
+        if (!ys.hasNext()) return Optional.empty();
+        y = ys.next();
+      } else if (x.equals(y)) {
+        return Optional.of(x);
+      } else {
+        if (!xs.hasNext() || !ys.hasNext()) {
+          return Optional.empty();
+        }
+        x = xs.next();
+        y = ys.next();
+      }
+    }
   }
 
   private Optional<Presses> winner3(Machine m) {
@@ -145,45 +196,56 @@ public class Day13_ClawContraption implements Solution {
       MinAndGap x = oneAxis(m.ax(), m.bx(), xZeros.get());
       MinAndGap y = oneAxis(m.ay(), m.by(), yZeros.get());
 
-      System.out.println(x);
-      System.out.println(y);
+      // System.out.println(x);
+      // System.out.println(y);
+
+      if (y.gap() == 0) { // || x.gap() == 0) {
+        //System.out.println("ZERO GAP");
+        System.out.println(m + " has zero gap");
+        return Optional.empty();
+      }
 
       var step = whichStep(x, y);
-      System.out.println("step: %d".formatted(step));
-      Range xRange = kRange(m.ax(), m.bx(), xZeros.get());
-      return Optional.of(presses(m.ax(), m.bx(), xZeros.get(), xRange.low() + step));
+      // System.out.println("step: %d".formatted(step));
+      var k = x.start() + step * x.sign();
+      if (!x.range().contains(k)) {
+        System.out.println("k " + k + " out of range");
+        return Optional.empty();
+      }
+
+      var p = presses(m.ax(), m.bx(), xZeros.get(), k);
+      if (!m.winner(p.a(), p.b())) {
+        System.out.println(m + " is not a winner with " + p);
+        return Optional.empty();
+      }
+
+      return Optional.of(p);
     }
   }
 
-  record MinAndGap(long min, long gap) {}
+  record MinAndGap(long min, long gap, long start, long sign, Range range) {}
 
   private MinAndGap oneAxis(long a, long b, Pair zeros) {
     var r = kRange(a, b, zeros);
     var lowPresses = presses(a, b, zeros, r.low());
     var highPresses = presses(a, b, zeros, r.high());
-    System.out.println("r: %s; lowPresses: %s; highPresses: %s".formatted(r, lowPresses, highPresses));
+    //System.out.println("r: %s; lowPresses: %s; highPresses: %s".formatted(r, lowPresses, highPresses));
 
     var gap = abs(presses(a, b, zeros, r.low() + 1).cost() - lowPresses.cost());
-    var minCost = min(lowPresses.cost(), highPresses.cost());
-    return new MinAndGap(minCost, gap);
+    var lowCost = lowPresses.cost();
+    var highCost = highPresses.cost();
+    if (lowCost < highCost) {
+      return new MinAndGap(lowCost, gap, r.low(), 1, r);
+    } else {
+      return new MinAndGap(highCost, gap, r.high(), -1, r);
+    }
   }
 
   private long whichStep(MinAndGap x, MinAndGap y) {
-    //   (mod (* (mod (nth-value 1 (extended-gcd gap-x gap-y)) gap-y) (- gap-y (rem (- min-x min-y) gap-y))) gap-y))
-
-
-    //   (mod (* (mod a gap-y) (- gap-y (rem (- min-x min-y) gap-y))) gap-y))
-
     var ext = extendedEuclidean(x.gap(), y.gap());
-    System.out.println(ext);
-
     var a = ext.x();
-    var b = a % y.gap();
     var c = y.gap() - ((x.min() - y.min()) % y.gap());
-
-    System.out.println("a: %s; b: %s; c: %s".formatted(a, b, c));
-
-    return floorMod((b * c), y.gap());
+    return floorMod((a * c), y.gap());
   }
 
   private Optional<Presses> winner(Machine m) {
@@ -203,24 +265,21 @@ public class Day13_ClawContraption implements Solution {
     return range(0, maxX)
       .boxed()
       .map(a -> {
-          //if (a % 100_000_000L == 0) System.out.print(".");
-          var xLeft = m.x - (m.ax * a);
-          var yLeft = m.y - (m.ay * a);
-          if (xLeft % m.bx == 0 && yLeft % m.by == 0) {
-            var bForX = xLeft / m.bx;
-            var bForY = yLeft / m.by;
-            if (bForX == bForY) {
-              return new Presses(a, bForX);
-            }
+        //if (a % 100_000_000L == 0) System.out.print(".");
+        var xLeft = m.x - (m.ax * a);
+        var yLeft = m.y - (m.ay * a);
+        if (xLeft % m.bx == 0 && yLeft % m.by == 0) {
+          var bForX = xLeft / m.bx;
+          var bForY = yLeft / m.by;
+          if (bForX == bForY) {
+            return new Presses(a, bForX);
           }
-          return null;
         }
-      )
+        return null;
+      })
       .filter(p -> p != null)
       .collect(minBy(Comparator.comparingLong(Presses::cost)));
   }
-
-
 
   private List<Machine> embiggen(List<Machine> machines) {
     long amt = 10000000000000L;
@@ -272,7 +331,6 @@ public class Day13_ClawContraption implements Solution {
     }
   }
 
-
   private Optional<Pair> zeros(long a, long b, long goal) {
     var ext = extendedEuclidean(a, b);
     if (goal % ext.gcd != 0) {
@@ -283,9 +341,13 @@ public class Day13_ClawContraption implements Solution {
     }
   }
 
-  record Pair(long x, long y) {};
+  record Pair(long x, long y) {}
 
-  record Range(long low, long high) {}
+  record Range(long low, long high) {
+    boolean contains(long value) {
+      return low <= value && value <= high;
+    }
+  }
 
   private Range kRange(long a, long b, Pair zeros) {
     var gcd = gcd(a, b);
@@ -300,11 +362,31 @@ public class Day13_ClawContraption implements Solution {
   }
 
   private Stream<Presses> allPresses(long a, long b, long goal) {
-    return zeros(a, b, goal).stream().flatMap(zeros -> {
+    return zeros(a, b, goal)
+      .stream()
+      .flatMap(zeros -> {
         var kRange = kRange(a, b, zeros);
         return rangeClosed(kRange.low, kRange.high).mapToObj(k -> presses(a, b, zeros, k));
       });
   }
 
+  private Stream<Presses> orderedPresses(long a, long b, long goal) {
+    return zeros(a, b, goal)
+      .stream()
+      .flatMap(zeros -> {
+        var kRange = kRange(a, b, zeros);
+        var low = presses(a, b, zeros, kRange.low());
+        var high = presses(a, b, zeros, kRange.high());
+        LongStream ks = low.cost() < high.cost()
+          ? fromTo(kRange.low(), kRange.high())
+          : fromTo(kRange.high(), kRange.low());
+        return ks.mapToObj(k -> presses(a, b, zeros, k));
+      });
+  }
 
+  private LongStream fromTo(long start, long end) {
+    long incr = (long) signum(end - start);
+    LongPredicate pred = incr == 1 ? i -> i <= end : i -> i >= end;
+    return LongStream.iterate(start, pred, i -> i + incr);
+  }
 }
